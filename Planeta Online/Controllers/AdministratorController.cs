5574b4459fb35bbf42d57ce93877b11e15d02a40
Andrii Kobzar <andrykobzar@gmail.com>
2015-11-06 22:52:59 +0200

    Added mock-up for generating pdf application for an event; added ActionLink, which should send an application to the administration of the university on SubmittedEvents page of Administrator controller

--- Planeta Online/Controllers/AdministratorController.cs
+++ Planeta Online/Controllers/AdministratorController.cs
@@ -41,7 +41,7 @@ namespace Planeta_Online.Controllers
             return View(db.EventApplications.ToList());
         }
         public ActionResult Confirm(int id)
-        {            
+        {
             EventApplication application = db.EventApplications.Find(id);
             if(application==null)
             {
@@ -60,13 +60,79 @@ namespace Planeta_Online.Controllers
             }
             
         }
-        private void GeneratePDF(Event @event)
+        public ActionResult SendApplication(int id)
         {
-            Document doc = new Document(PageSize.LETTER, 10, 10, 42, 35);
-            //PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream());
-            doc.Open();
-            Paragraph p = new Paragraph("Podannya");
-            doc.Close();
+            EventApplication _event = db.EventApplications.Find(id);
+            GeneratePDF(_event);
+            return RedirectToAction("Index");
+        }
+        private void GeneratePDF(EventApplication @event)
+        {
+            Document document = new Document(PageSize.A4, 72, 65, 72, 65);
+
+            string filename = "application" + @event.Id + DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString() + ".pdf";
+            filename = "~\\Applications\\" + filename;
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(Server.MapPath(filename), FileMode.Create));
+            document.AddAuthor("RussianPDFtest");
+            document.AddTitle("Подтверждение бронирования");
+            document.AddCreationDate();
+
+            string sylfaenpath = Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\sylfaen.ttf";
+            BaseFont sylfaen = BaseFont.CreateFont(sylfaenpath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font head = new Font(sylfaen, 12f, Font.NORMAL, BaseColor.BLUE);
+            Font normal = new Font(sylfaen, 10f, Font.NORMAL, BaseColor.BLACK);
+            Font underline = new Font(sylfaen, 10f, Font.UNDERLINE, BaseColor.BLACK);
+
+            document.Open();
+            document.Add(new Paragraph("Подання", head));
+            document.Add(new Paragraph(" Бугрову Володимиру Анатолійовичу", normal));
+            document.Add(new Paragraph("Просимо дозволу провести захід ЄЄЄ у диско-клубі \"Планета\" ", normal));
+            document.Add(new Paragraph("Мы подтверждаем бронирование, содержащее следующую информацию:", underline));
+            document.Add(new Paragraph(" ", normal));
+            PdfPTable table1 = new PdfPTable(2);
+            table1.TotalWidth = document.PageSize.Width - 72f - 65f;
+            table1.LockedWidth = true;
+            float[] widths1 = new float[] { 1f, 4f };
+            table1.SetWidths(widths1);
+            table1.HorizontalAlignment = 0;
+            PdfPCell table1cell11 = new PdfPCell(new Phrase("Объект:", normal));
+            table1cell11.Border = 0;
+            table1.AddCell(table1cell11);
+            PdfPCell table1cell12 = new PdfPCell(new Phrase("Ferienhaus 'Waldesruh'", normal));
+            table1cell12.Border = 0;
+            table1.AddCell(table1cell12);
+            PdfPCell table1cell21 = new PdfPCell(new Phrase("Адрес:", normal));
+            table1cell21.Border = 0;
+            table1.AddCell(table1cell21);
+            PdfPCell table1cell22 = new PdfPCell(new Phrase("15344 Strausberg, Am Marienberg 45", normal));
+            table1cell22.Border = 0;
+            table1.AddCell(table1cell22);
+            PdfPCell table1cell31 = new PdfPCell(new Phrase("Номер объекта:", normal));
+            table1cell31.Border = 0;
+            table1.AddCell(table1cell31);
+            PdfPCell table1cell32 = new PdfPCell(new Phrase("czr04012012", normal));
+            table1cell32.Border = 0;
+            table1.AddCell(table1cell32);
+            PdfPCell table1cell41 = new PdfPCell(new Phrase("Дата заезда:", normal));
+            table1cell41.Border = 0;
+            table1.AddCell(table1cell41);
+            PdfPCell table1cell42 = new PdfPCell(new Phrase("12.02.2012", normal));
+            table1cell42.Border = 0;
+            table1.AddCell(table1cell42);
+            PdfPCell table1cell51 = new PdfPCell(new Phrase("Дата выезда:", normal));
+            table1cell51.Border = 0;
+            table1.AddCell(table1cell51);
+            PdfPCell table1cell52 = new PdfPCell(new Phrase("18.02.2012", normal));
+            table1cell52.Border = 0;
+            table1.AddCell(table1cell52);
+            PdfPCell table1cell61 = new PdfPCell(new Phrase("Человек:", normal));
+            table1cell61.Border = 0;
+            table1.AddCell(table1cell61);
+            PdfPCell table1cell62 = new PdfPCell(new Phrase("5", normal));
+            table1cell62.Border = 0;
+            table1.AddCell(table1cell62);
+            document.Add(table1);
+            document.Close();
         }
         public ActionResult Visitors(int? id)
         {
