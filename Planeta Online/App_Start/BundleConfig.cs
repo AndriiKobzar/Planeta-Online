12e7e51d0505238b59d3369a890e0fe293af0ac5
Andrii Kobzar <andrykobzar@gmail.com>
2015-12-06 20:50:37 +0200

    First version of calendar with jQuery fullcalendar

--- Planeta Online/App_Start/BundleConfig.cs
+++ Planeta Online/App_Start/BundleConfig.cs
@@ -26,6 +26,17 @@ namespace Planeta_Online
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //Calendar css file
+            bundles.Add(new StyleBundle("~/Content/fullcalendarcss").Include(
+                     "~/Content/themes/jquery.ui.all.css",
+                     "~/Content/fullcalendar.css"));
+
+            //Calendar Script file
+
+            bundles.Add(new ScriptBundle("~/bundles/fullcalendarjs").Include(
+                      "~/Scripts/jquery-ui-1.10.4.min.js",
+                      "~/Scripts/fullcalendar.min.js"));
+
         }
     }
 }
