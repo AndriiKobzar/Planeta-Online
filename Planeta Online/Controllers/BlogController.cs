317a4e51179cbca70ea9ce168b26546f300a8c07
Andrii Kobzar <andrykobzar@gmail.com>
2016-01-27 23:20:56 +0200

    Added BlogController and BlogPost model

---
+++ Planeta Online/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Planeta_Online.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Planeta_Online.Controllers
+{
+    public class BlogController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View(db.Blog.ToList());
+        }
+    }
+}
---
+++ Planeta Online/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planeta_Online.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+    }
+}
