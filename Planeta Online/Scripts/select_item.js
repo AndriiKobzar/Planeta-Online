dabbc95b8f87dc3b457e8a107383e149940443eb
Andrii Kobzar <andrykobzar@gmail.com>
2016-02-15 17:53:39 +0200

    Deleted language scripts from ckeditor; added script to higlight active navbar item

---
+++ Planeta Online/Scripts/select_item.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    var currentPageAdress = window.location.href;
+    var requestPath = currentPageAdress.substr(currentPageAdress.indexOf('/', 9));
+    if (requestPath == '/' || requestPath.includes('Events')) {
+        $('#events').addClass('active');
+    }
+    else if (requestPath.includes('Books')) {
+        $('#books').addClass('active')
+    }
+    else if (requestPath.includes('Home/About')) {
+        $('#about').addClass('active')
+    }
+    else if (requestPath.includes('Administrator')) {
+        $('#admin').addClass('active')
+    }
+    else if (requestPath.includes('Blog')) {
+        $('#blog').addClass('active')
+    }
+}); 
