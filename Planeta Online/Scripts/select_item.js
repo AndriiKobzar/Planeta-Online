50d17593cb2a9c0a7aca08a3fafc22c14a4a12b2
Andrii Kobzar <andrykobzar@gmail.com>
2016-02-17 20:26:22 +0200

    Minor bug fixes

--- Planeta Online/Scripts/select_item.js
+++ Planeta Online/Scripts/select_item.js
@@ -1,7 +1,11 @@
 ﻿$(document).ready(function () {
     var currentPageAdress = window.location.href;
     var requestPath = currentPageAdress.substr(currentPageAdress.indexOf('/', 9));
-    if (requestPath == '/' || requestPath.includes('Events')) {
+
+    if (requestPath.includes('Administrator')) {
+        $('#admin').addClass('active')
+    }
+    else if (requestPath == '/' || requestPath.includes('Events')) {
         $('#events').addClass('active');
     }
     else if (requestPath.includes('Books')) {
@@ -10,10 +14,8 @@
     else if (requestPath.includes('Home/About')) {
         $('#about').addClass('active')
     }
-    else if (requestPath.includes('Administrator')) {
-        $('#admin').addClass('active')
-    }
+    
     else if (requestPath.includes('Blog')) {
         $('#blog').addClass('active')
     }
-}); 
+});
